f1be081d111b4a0e5a58eafb512b8a3cc1c7bb3e
Dmitri Brereton <mitrikyle@yahoo.com>
2015-12-25 01:54:58 -0400

    added basic functionality

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 module.exports = {
     todo: function() {
         return "TODO: Everything";
+    },
+
+    // splits string up by keywords into location,date,time
+    separateByKeyword: function(str) {
+        var event = {
+            date: 0,
+            time: 0,
+            location: '',
+        };
+        var dateWords = [];
+        var timeWords = [];
+        var locationWords = [];
+        str = str.split(' ');
+        // search for keywords
+        for(var i=0;i<str.length;i++){
+            if(str[i] == 'on'){
+                dateWords.push(str[i+1]);
+            } else if (str[i] == 'in'){
+                locationWords.push(str[i+1]);
+            }
+            else if (str[i] == 'at') {
+                // because at can be a location or a time, check to see if there's a number next
+                // If there is a number then it's a time, else assume it's a location
+                if(isNaN(str[i+1])){
+                    // not a number, therefore locaton
+                    locationWords.push(str[i+1]);
+                } else {
+                    // is a number, is a time
+                    timeWords.push(str[i+1]); //TODO times are more than one words, 9:00PM
+                }
+            }
+        }
+
+        console.log("date words: " + dateWords +"\n");
+        console.log("time words: " + timeWords + "\n");
+        console.log("locatio nwords: " + locationWords + "\n");
     }
 };
